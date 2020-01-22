f7ba7f753e81715a4af6005250cb0aaa0c5c55bf
jwilly117 <jakewilliams117@gmail.com>
2020-01-21 19:07:34 -0500

    adding some js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function renderTodos(list) {
+    $("#to-dos").empty(); // empties out the html
+
+    // Render our todos to the page
+    for (var i = 0; i < list.length; i++){
+        
+    }
+}
